582ba2ac8b48d3ed224d9a591f615f134702c93e
ramatoulaye <ramaba389@gmail.com>
2019-10-09 19:42:05 +0000

    mod 4

--- controler/db.php
+++ controler/db.php
@@ -3,16 +3,10 @@
    $dsn = 'mysql:host=localhost;dbname=gesticketsa;port=3306;charset=utf8';
    //b) Selectionner BD
    try {
-<<<<<<< HEAD
-      $pdo = new PDO($dsn, 'ramaba' , 'Saliaba@rm.2019');
-      
-=======
-      $pdo = new PDO($dsn, 'ouze' , 'malamine10');
->>>>>>> 499cc36c2d7fa2bac4dbf169174e29da3a0a59b3
+        $pdo = new PDO($dsn, 'ramaba' , 'Saliaba@rm.2019');
       }
       catch (PDOException $exception) {
-         
-         //mail('fauxmail@votremail.com', 'PDOException', $exception->getMessage());
+         mail('fauxmail@votremail.com', 'PDOException', $exception->getMessage());
          exit('Erreur de connexion à la base de données');  
    }
 ?>
