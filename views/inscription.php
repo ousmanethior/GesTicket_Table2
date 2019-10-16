c983e23cdcff18fb55cd454a984ad2104c6ebc44
ramatoulaye <ramaba389@gmail.com>
2019-10-16 08:25:58 +0000

    inscription bootstrap

--- views/inscription.php
+++ views/inscription.php
@@ -12,18 +12,19 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-      <div class="container-fluid">
+     <div class="container-fluid">
        <!-- menu -->
           <div class="row bg-primary">
-            <div class="col-3">
-                <img src="../public/img/as.jpg" class="tiket-img-fluid  rounded-circle " alt="">
+            <div class="col-3 text-center">
+                <img src="../public/img/as.jpg" class="tiket-img-fluid  rounded-circle" alt="">
+                <p class="text-secondary font-weight-bold">Astou Ndiaye</p> 
             </div>
             <div class="col">
               <nav class="navbar navbar-expand-sm navbar-dark float-right">
                     <div class="collapse navbar-collapse col-sm-2" id="navbarSupportedContent">
                         <ul class="navbar-nav ml-auto">
                         <li class="nav-item dropdown">
-                            <a class="nav-link " href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <a class="nav-link text-secondary font-weight-bold" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Référentiel
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -32,7 +33,7 @@
                             </div>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link " href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <a class="nav-link text-secondary font-weight-bold" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Utilisateur
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -41,7 +42,7 @@
                             </div>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link " href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <a class="nav-link text-secondary font-weight-bold" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Session
                             </a>
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
@@ -57,51 +58,143 @@
             
             </div>
           </div>
-        <!-- Formulaire-->
+        <!-- Formulaire--> 
           <div class="row">
                 <div class="col">
                   <img src="../public/img/sa logo.png" class="img-fluid mt-2" alt="">
                 </div>
                 <div class="col-6">
-                <h2 class="text-center text-primary mt-2">Inscription</h2>
-                <form class="bg-primary">
-                
-                <div class="form-group row">
-                    <label for="inputPassword" class="col-sm-2 col-form-label">Nom</label>
-                    <div class="col-sm-10">
-                    <input type="text" class="form-control border border-primary form-control-sm" id="inputPassword" placeholder="Password">
+                   <h2 class="text-center text-info mt-5 mb-5">Inscription</h2>
+                   <form class="bg-primary rounded ">
+                        <div class="form-group row">
+                            <label for="staticEmail" class="col-sm-2 col-form-label text-secondary font-weight-bold">
+                             Nom
+                            </label>
+                            <div class="col-sm-7">
+                            <input type="text" class="form-control form-control-sm" id="inputPassword" placeholder="THIOR">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="inputPassword" class="col-sm-2 col-form-label text-secondary font-weight-bold">
+                             Prénom
+                            </label>
+                            <div class="col-sm-7">
+                            <input type="text" class="form-control form-control-sm" id="inputPassword" placeholder="Ousmane">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="staticEmail" class="col-sm-2 col-form-label text-secondary font-weight-bold">
+                                CIN/Passeport
+                            </label>
+                            <div class="col-sm-7">
+                            <input type="text" class="form-control form-control-sm" id="inputPassword" placeholder="00edhjh45">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="inputPassword" class="col-sm-2 col-form-label text-secondary font-weight-bold">
+                                Email
+                            </label>
+                            <div class="col-sm-7">
+                            <input type="email" class="form-control form-control-sm" id="inputPassword" placeholder="exemple@gmail">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="inputPassword" class="col-sm-2 col-form-label text-secondary font-weight-bold">
+                                Téléphone
+                            </label>
+                            <div class="col-sm-7">
+                            <input type="text" class="form-control form-control-sm" id="inputPassword" placeholder="77 777 77">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="inputPassword" class="col-sm-2 col-form-label text-secondary font-weight-bold">
+                                Référentiel
+                            </label>
+                            <div class="col-sm-7">
+                                <select class="form-control form-control-sm">
+                                <option>Small select</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col"></div>
+                            <div class="col-6">
+                                <button type="reset" class="btn btn-primary ">Annuler</button>
+                                <button type="submit" class="btn btn-primary">Inscrire</button>
+                            </div>
+                            <div class="col"></div>
+                        </div>
+                    </form>
                 </div>
-                </div>
-                </form>
-                </div>
-                <div class="col"></div>
+              <div class="col"></div>
           </div>
         <!-- Footer -->
-          <div class="row"></div>
+          <div class="row bg-primary  row-footer">
+             csdcs
+          </div>
       </div>
 
 
 
       <style type="text/css">
        .tiket-img-fluid {
-            max-width: 15%;
+            max-width: 7%;
             height: auto;
         }
-                    .img-fluid {
-                max-width: 30%;
-                height: auto;
-            }
+        .img-fluid {
+            max-width: 30%;
+            height: auto;
+        }
         .bg-primary {
           background-color: #068689 !important;
-                }
-                .text-primary {
-               color: #068689 !important;
-}
-.border-primary {
-
-border-color: #068689 !important;
-
-}   
+        }
+        .bg-secondary {
+          background-color: #e0f0f0 !important;
+        }
+        .bg-info {
+          background-color: #ee7f22 !important;
+        }
+        .text-primary {
+           color: #068689 !important;
+         }
+        .text-primary {
+           color: #068689 !important;
+         }
+         .text-info {
+           color: #ee7f22 !important;
+         }
+         .text-secondary {
+           color: #e0f0f0 !important;
+         }
+        .border-primary {
+          border-color: #068689 !important;
+        } 
+        .btn-primary {
+            color: #ee7f22;
+            background-color: #fff;
+            border-color: #e0f0f0;
+        } 
+        .col-form-label {
+            padding-top: 10px;
+            padding-left: 80px;
+        }
+        .form-control-sm {
+            margin-top: 9px;
+            margin-left: 100px;
+        }
+        .rounded {
+           border-radius: 50px !important;
+        }
+        .btn {
+            font-weight: 810;
+            border-radius: 30px;
+            width: 130px;
+        }
+        .row-footer {
+           height:80%;
+        }
+ 
       </style>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
