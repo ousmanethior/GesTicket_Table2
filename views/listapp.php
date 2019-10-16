87ce1152bce2fb5adcdb8ddfbf12442be2611b9d
Ousmane Thior <thior15@hotmail.com>
2019-10-16 11:06:56 +0000

    recupinscrip

--- views/listapp.php
+++ views/listapp.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>listapp</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="../public/css/style.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-</head>
-<body>
+      <title>listapp</title>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+          <link rel="stylesheet" type="text/css" href="../public/css/style.css">
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+      </head>
+  <body>
     <header>
     
         <nav style="float:right" position="relative">
@@ -21,24 +21,34 @@
             </ul>
         </nav>
     </header>
-<div class="container">
-  <h1 style="margin-left:200px; margin-top:100px; color:#068689;">Liste des apprenants de la Sonatel Academy</h1>
+  <div class="container">
+  <img src="../public/img/sa logo.png">
+    <h1 style="margin-left:200px; margin-top:5px; color:#068689;">Liste des apprenants de la Sonatel Academy</h1>
           
-  <table class="table table-striped" style= margin:50px;>
-    <thead>
-      <tr>
-        <th>Prénom</th>
-        <th>Nom</th>
-        <th>Email</th>
-        <th>Référentiel</th>
-        <th>CIN/PASSEPORT</th>
-        <th>Téléphone</th>
-        <th>Matricule</th>
+      <table class="table table-striped" style= margin:10px;>
+       <thead>
+          <tr>
+              <th>Prénom</th>
+              <th>Nom</th>
+              <th>Email</th>
+              <th>Référentiel</th>
+              <th>CIN/PASSEPORT</th>
+              <th>Téléphone</th>
+              <th>Matricule</th>
 
-      </tr>
-    </thead>
+          </tr>
+      </thead>
+          <tr>
+               <td><?php echo $user['prenom'];?></td>
+               <td><?php echo $user['nom'];?></td>
+               <td><?php echo $user['email'];?></td>
+               <td><?php echo $user['referentiel'];?></td>
+               <td><?php echo $user['cinpassport'];?></td>
+               <td><?php echo $user['telephone'];?></td>
+               <td><?php echo $user['matricule'];?></td>
+          </tr>
     
-</div>
+    </div>
 
-</body>
+   </body>
 </html>
